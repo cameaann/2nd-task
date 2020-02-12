2f5a79f98c543c1abe5f11b59b17a1b63e0803e8
cameaann <cameaann@gmail.com>
2020-02-13 00:03:12 +0300

    added sass to project

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
 
 app.set('view engine', 'pug');
 
-app.get('/', (req, res)=>{
-    res.render('index');
+app.get('/', async (req, res)=>{
+    const query = await axios.get('http://localhost:3001/rooms');
+    res.render('index', { rooms: query.data}); 
+    res.render('ui-kit');
 });
 
+app.use(express.static(__dirname + '/public'));
 
 const httpServer = app.listen(3000, () => {
     console.log("HTTP REST API Server running at http://localhost:" + httpServer.address().port);
